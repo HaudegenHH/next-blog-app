e1e704807f66a23c82b71d73491f1ced1ae475e2
HaudegenHH <einsundeins79@gmx.de>
2022-10-09 00:43:48 +0200

    scaffold project

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora error,
+        nihil dolor aut, quia perspiciatis magni veritatis sit soluta culpa
+        earum sint, corrupti hic veniam voluptatem. Voluptatum incidunt eum
+        adipisci!
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora error,
+        nihil dolor aut, quia perspiciatis magni veritatis sit soluta culpa
+        earum sint, corrupti hic veniam voluptatem. Voluptatum incidunt eum
+        adipisci!
+      </p>
+    </div>
+  );
+};
+
+export default About;
